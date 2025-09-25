fb4a3dc086137d56d9a890972dfa156692f8e5b9
simi chakma <simichakma139@gmail.com>
2025-09-25 18:55:21 +0600

    Delete Echogpt_Loading_Testing directory

--- Echogpt_Loading_Testing/echnogpt.py/tests/test_home_page.py
+++
@@ -1,8 +0,0 @@
-from pages.home_page import HomePage
-
-def test_navigate(driver):
-    home = HomePage(driver)
-    home.visit("https://echogpt.live")
-
-    home.click("image")
-    assert "image" in driver.current_url.lower()
